8dc114e234e83b355259b274204fb502c9dab310
Danilo Santos <danilo3990@gmail.com>
2023-06-27 21:45:49 -0300

    :new: Criaçao Repositorio Alura
    criação repositorio para desafios da Alura

---
+++ conversor_de_moedas/script/script.js
@@ -0,0 +1,6 @@
+var valorEmDolar = 30;
+var cotacaoDolar = 5.32;
+var valorConvertido = (valorEmDolar * cotacaoDolar);
+valorConvertido = valorConvertido.toFixed(2);
+
+console.log("R$ " + valorConvertido);
